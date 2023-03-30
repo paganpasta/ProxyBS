88ab6ff0644c22e4f92d7a7be9dfc9acd16ce2da
paganpasta <aditya.91.singh@gmail.com>
2023-03-30 10:27:03 +0100

    added headers for wandb

--- scripts/train_val.sh
+++ scripts/train_val.sh
@@ -1,2 +1,5 @@
-python main_val.py --data cifar10 --data_path /home/coco/datasets/ --model wrn28 \
---method val  --group DEBUG
+export KEY=
+export ENTITY=paganpasta
+export PROJECT=ProxyBS
+
+python main_val.py --data cifar10 --data_path /home/coco/datasets/ --model wrn28 --method val  --group cifar10
